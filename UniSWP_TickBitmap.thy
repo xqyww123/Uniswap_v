6127c0cb845d7be095654a0502882329da95b846
Qiyuan Xu <xqyww123@gmail.com>
2023-04-01 22:47:11 +0800

    Tickmap

--- UniSWP_TickBitmap.thy
+++ UniSWP_TickBitmap.thy
@@ -7,8 +7,13 @@ Get fiction relation from \<phi>-type relation.*)
 
 type_synonym tick_map = \<open>tick \<Rightarrow> bool\<close>
 
-resource_space tick_bitmap =
-  ticks :: \<open>UNIV::ticks_resource set\<close> (nonsepable_mono_resource)
-  by (standard; simp add: set_eq_iff image_iff)
+locale Tickmap_spec =
+  fixes Tickmap :: \<open>(fiction, tick_map) \<phi>\<close>
+    and op_get_tickmap :: \<open>(VAL, VAL) proc'\<close>
+  assumes op_get_tickmap:
+    \<open> \<p>\<r>\<o>\<c> op_get_tickmap ri \<lbrace> m \<Ztypecolon> Tickmap\<heavy_comma> i \<Ztypecolon> \<v>\<a>\<l>[ri] Tick
+                          \<longmapsto> m \<Ztypecolon> Tickmap\<heavy_comma> m i \<Ztypecolon> \<v>\<a>\<l> \<bool>\<rbrace> \<close>
+
+
 
 end
