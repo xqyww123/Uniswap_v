b609defbfc06447a423c8abfcd716145f207e54f
Qiyuan Xu <xqyww123@gmail.com>
2023-04-03 21:14:15 +0800

    computeSwapStep

--- Uniswap_SqrtPriceMath.thy
+++ Uniswap_SqrtPriceMath.thy
@@ -3,6 +3,71 @@ theory Uniswap_SqrtPriceMath
 begin
 
 proc getAmount0Delta':
+  \<comment> \<open>In this version we don't consider precision at all, so there is no rounding.
+      Though it doesn't mean \<phi>-system currently has no ability to do precision analysis,
+      I am thinking of a general framework in \<phi>-system boosting precision analysis,
+      maybe based on relative error.\<close>
+  input \<open>rA \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> rB \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  premises \<open>0 < rA \<and> 0 < rB \<and> 0 \<le> liq\<close>
+  output \<open>\<bar>liq / rA - liq / rB\<bar> \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket> 
+    $rA $rB \<rightarrow> var rA, rB ;;
+    if \<open>$rA > $rB\<close> \<medium_left_bracket> $rB $rA \<rightarrow> rA, rB \<medium_right_bracket>. \<medium_left_bracket> \<medium_right_bracket>. ;; 
+
+    $liq \<rightarrow> val numerator1 ;;
+    \<open>$rB - $rA\<close> \<rightarrow> val numerator2 ;;
+
+    require (\<open> $rA > 0 \<close>) ;;
+
+    \<open>$numerator1 * $numerator2 / $rA / $rB\<close>
+  \<medium_right_bracket> using \<phi> apply auto
+    apply (smt (verit, ccfv_SIG) diff_divide_distrib diff_frac_eq divide_strict_left_mono mult_pos_pos right_diff_distrib)
+    by (smt (verit) diff_frac_eq divide_nonneg_nonneg right_diff_distrib' split_mult_pos_le) .
+
+proc getAmount0Delta:
+  input \<open>rA \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> rB \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  premises \<open>0 < rA \<and> rA \<le> rB\<close>
+  output \<open>(liq / rA - liq / rB) \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket> if \<open>$liq < 0\<close>
+    \<medium_left_bracket> neg (getAmount0Delta'($rA, $rB, \<open>-$liq\<close>)) \<medium_right_bracket>.
+    \<medium_left_bracket> getAmount0Delta'($rA, $rB, $liq) \<medium_right_bracket>. ;;
+  \<medium_right_bracket> using \<phi> apply auto
+    apply (smt (z3) frac_le minus_divide_left)
+    by (simp add: frac_le) .
+
+proc getAmount1Delta':
+  \<comment> \<open>In this version we don't consider precision at all, so there is no rounding.
+      Though it doesn't mean \<phi>-system currently has no ability to do precision analysis,
+      I am thinking of a general framework in \<phi>-system boosting precision analysis,
+      maybe based on relative error.\<close>
+  input \<open>rA \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> rB \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  premises \<open>0 < rA \<and> 0 < rB \<and> 0 \<le> liq\<close>
+  output \<open>\<bar>liq * (rB - rA)\<bar> \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket> 
+    $rA $rB \<rightarrow> var rA, rB ;;
+    if \<open>$rA > $rB\<close> \<medium_left_bracket> $rB $rA \<rightarrow> rA, rB \<medium_right_bracket>. \<medium_left_bracket> \<medium_right_bracket>. ;; 
+
+    \<open>$liq * ($rB - $rA)\<close>
+  \<medium_right_bracket> using \<phi> apply auto
+    by (simp add: abs_mult) .
+
+proc getAmount1Delta:
+  input \<open>rA \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> rB \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  premises \<open>0 < rA \<and> rA \<le> rB\<close>
+  output \<open>liq * (rB - rA) \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket> if \<open>$liq < 0\<close>
+    \<medium_left_bracket> neg (getAmount1Delta' ($rA, $rB, \<open>-$liq\<close>)) \<medium_right_bracket>.
+    \<medium_left_bracket> getAmount1Delta' ($rA, $rB, $liq) \<medium_right_bracket>. ;;
+  \<medium_right_bracket> using \<phi> apply auto
+    by (smt (verit, ccfv_threshold) zero_less_mult_iff) .
+
+
+
+proc getAmount0Delta'_rounded:
   input \<open>rA \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> rB \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> roundUp \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
   premises \<open>0 < rA \<and> 0 < rB\<close>
   output \<open>(if roundUp then nat \<lceil> \<bar>real liq / rA - real liq / rB\<bar> \<rceil>
@@ -35,15 +100,15 @@ proc getAmount0Delta':
     apply (smt (verit, best) diff_frac_eq divide_nonneg_nonneg mult.commute mult_nonneg_nonneg of_nat_0_le_iff right_diff_distrib')
     by (smt (verit, ccfv_threshold) diff_frac_eq divide_nonneg_pos mult.commute of_nat_0_le_iff right_diff_distrib zero_less_mult_iff) .
 
-proc getAmount0Delta:
+proc getAmount0Delta_rounded:
   input \<open>rA \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> rB \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<int>\<close>
   premises \<open>0 < rA \<and> rA < rB\<close>
   output \<open>(if liq < 0 then \<lfloor> real_of_int liq / rB - real_of_int liq / rA \<rfloor>
                       else \<lceil> real_of_int liq / rA - real_of_int liq / rB \<rceil>) \<Ztypecolon> \<v>\<a>\<l> \<int>\<close>
   is [routine]
   \<medium_left_bracket> if \<open>$liq < 0\<close>
-    \<medium_left_bracket> $rA $rB \<open>-$liq\<close> \<open>False\<close> getAmount0Delta' \<medium_right_bracket>.
-    \<medium_left_bracket> $rA $rB $liq \<open>True\<close> getAmount0Delta' \<medium_right_bracket>. ;;
+    \<medium_left_bracket> $rA $rB \<open>-$liq\<close> \<open>False\<close> getAmount0Delta'_rounded \<medium_right_bracket>.
+    \<medium_left_bracket> $rA $rB $liq \<open>True\<close> getAmount0Delta'_rounded \<medium_right_bracket>. ;;
   \<medium_right_bracket> using \<phi> apply auto
     apply (smt (verit, ccfv_SIG) divide_minus_left frac_less2 of_int_less_0_iff)
     by (metis abs_of_nonneg diff_less_0_iff_less div_0 frac_less2 linorder_not_less not_less_iff_gr_or_eq of_int_0 of_int_pos) .
@@ -51,6 +116,85 @@ proc getAmount0Delta:
 
 
 proc getNextSqrtPriceFromAmount0:
+  input \<open>sp \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> amount \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> add \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
+  premises \<open>0 < liq \<and> 0 < sp\<close>
+  output \<open> (if add then liq / (liq / sp + amount) else liq / (liq / sp - amount)) \<Ztypecolon> \<v>\<a>\<l> \<real> \<close>
+  is [routine]
+  \<medium_left_bracket>
+    if \<open>$amount = 0\<close> \<medium_left_bracket> return ($sp) \<medium_right_bracket>. \<medium_left_bracket> \<medium_right_bracket>. ;; (*Note, we have \<open>0 < amount\<close> in thm \<phi> now*)
+    $liq \<rightarrow> val numerator1 ;;
+    if \<open>$add\<close> \<medium_left_bracket>
+      \<open>$amount * $sp\<close> \<rightarrow> val product ;;
+      if \<open>$product / $amount = $sp\<close> \<medium_left_bracket>
+        \<open>$numerator1 + $product\<close> \<rightarrow> val denominator ;;
+        if \<open>$denominator \<ge> $numerator1\<close> \<medium_left_bracket>
+          return (\<open>$numerator1 * $sp / $denominator\<close>)
+            affirm using \<phi> by (simp add: add_frac_num) ;; (*TODO: add simplification before \<medium_right_bracket>. so this ;; can be removed*)
+        \<medium_right_bracket>. \<medium_left_bracket> \<medium_right_bracket>.
+      \<medium_right_bracket>. \<medium_left_bracket> \<medium_right_bracket>. ;;
+      return ($numerator1 div (\<open>$numerator1 / $sp\<close> add ($amount)))
+    \<medium_right_bracket>. \<medium_left_bracket>
+      \<open>$amount * $sp\<close> \<rightarrow> val product ;;
+      require (\<open>$product / $amount = $sp\<close>) ;;
+      \<open>$numerator1 - $product\<close> \<rightarrow> val denominator ;;
+      return (\<open>$numerator1 * $sp / $denominator\<close>)
+      affirm using \<phi> by (simp add: add_divide_eq_if_simps(5))
+    \<medium_right_bracket>. ;;
+  \<medium_right_bracket>. .
+
+proc getNextSqrtPriceFromAmount1:
+  input \<open>sp \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> amount \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> add \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
+  premises \<open>\<not> add \<longrightarrow> amount / liq < sp\<close>
+  output \<open>(if add then sp + amount / liq else sp - amount / liq) \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket>
+    if \<open>$add\<close> \<medium_left_bracket>
+      \<open>$amount / $liq\<close> \<rightarrow> val quotient ;;
+      return (\<open>$sp + $quotient\<close>)
+    \<medium_right_bracket>. \<medium_left_bracket>
+      \<open>$amount / $liq\<close> \<rightarrow> val quotient ;;
+      require (\<open>$sp > $quotient\<close>) ;;
+      return (\<open>$sp - $quotient\<close>)
+    \<medium_right_bracket>. ;;
+  \<medium_right_bracket>. .
+
+proc getNextSqrtPriceFromInput:
+  input \<open>sp \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> amountIn \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> zeroForOne \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
+  premises \<open>0 < sp \<and> 0 < liq\<close>
+  output \<open>(if zeroForOne then liq / (liq / sp + amountIn) else sp + amountIn / liq) \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket>
+    require (\<open>$sp > 0\<close>) ;;
+    require (\<open>$liq > 0\<close>) ;;
+    if \<open>$zeroForOne\<close> \<medium_left_bracket>
+      getNextSqrtPriceFromAmount0 ($sp, $liq, $amountIn, \<open>True\<close>)
+    \<medium_right_bracket>. \<medium_left_bracket>
+      getNextSqrtPriceFromAmount1 ($sp, $liq, $amountIn, \<open>True\<close>)
+    \<medium_right_bracket>. return (*it returns the result of the branch*)
+  \<medium_right_bracket>. .
+
+proc getNextSqrtPriceFromOutput:
+  input \<open>sp \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> amountOut \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> zeroForOne \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
+  premises \<open>0 < sp \<and> 0 < liq\<close>
+      and  \<open>zeroForOne \<longrightarrow> amountOut / liq < sp\<close>
+  output \<open>(if zeroForOne then sp - amountOut / liq else liq / (liq / sp - amountOut)) \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket>
+    require (\<open>$sp > 0\<close>) ;;
+    require (\<open>$liq > 0\<close>) ;;
+    if \<open>$zeroForOne\<close> \<medium_left_bracket>
+      getNextSqrtPriceFromAmount1 ($sp, $liq, $amountOut, \<open>False\<close>)
+    \<medium_right_bracket>. \<medium_left_bracket>
+      getNextSqrtPriceFromAmount0 ($sp, $liq, $amountOut, \<open>False\<close>)
+    \<medium_right_bracket>. return (*it returns the result of the branch*)
+  \<medium_right_bracket>. .
+
+
+
+
+paragraph \<open>Liquidity formalization using integers\<close>
+
+proc getNextSqrtPriceFromAmount0_int:
   input \<open>sp \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> amount \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> add \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
   premises \<open>0 < liq \<and> 0 < sp\<close>
   output \<open> (if add then liq / (liq / sp + amount) else liq / (liq / sp - amount)) \<Ztypecolon> \<v>\<a>\<l> \<real> \<close>
@@ -77,7 +221,7 @@ proc getNextSqrtPriceFromAmount0:
     \<medium_right_bracket>. ;;
   \<medium_right_bracket>. .
 
-proc getNextSqrtPriceFromAmount1:
+proc getNextSqrtPriceFromAmount1_int:
   input \<open>sp \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> amount \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> add \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
   premises \<open>\<not> add \<longrightarrow> real amount / real liq < sp\<close>
   output \<open>(if add then sp + amount / liq else sp - amount / liq) \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
@@ -93,7 +237,7 @@ proc getNextSqrtPriceFromAmount1:
     \<medium_right_bracket>. ;;
   \<medium_right_bracket>. .
 
-proc getNextSqrtPriceFromInput:
+proc getNextSqrtPriceFromInput_int:
   input \<open>sp \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> amountIn \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> zeroForOne \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
   premises \<open>0 < sp \<and> 0 < liq\<close>
   output \<open>(if zeroForOne then liq / (liq / sp + real amountIn) else sp + amountIn / liq) \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
@@ -102,9 +246,25 @@ proc getNextSqrtPriceFromInput:
     require (\<open>$sp > 0\<close>) ;;
     require (\<open>$liq > 0\<close>) ;;
     if \<open>$zeroForOne\<close> \<medium_left_bracket>
-      getNextSqrtPriceFromAmount0 ($sp, $liq, $amountIn, \<open>True\<close>)
+      getNextSqrtPriceFromAmount0_int ($sp, $liq, $amountIn, \<open>True\<close>)
     \<medium_right_bracket>. \<medium_left_bracket>
-      getNextSqrtPriceFromAmount1 ($sp, $liq, $amountIn, \<open>True\<close>)
+      getNextSqrtPriceFromAmount1_int ($sp, $liq, $amountIn, \<open>True\<close>)
+    \<medium_right_bracket>. return (*it returns the result of the branch*)
+  \<medium_right_bracket>. .
+
+proc getNextSqrtPriceFromOutput_int:
+  input \<open>sp \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> liq \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> amountOut \<Ztypecolon> \<v>\<a>\<l> \<nat>\<heavy_comma> zeroForOne \<Ztypecolon> \<v>\<a>\<l> \<bool>\<close>
+  premises \<open>0 < sp \<and> 0 < liq\<close>
+      and  \<open>real amountOut / real liq < sp\<close>
+  output \<open>(if zeroForOne then sp - amountOut / liq else liq / (liq / sp - real amountOut)) \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket>
+    require (\<open>$sp > 0\<close>) ;;
+    require (\<open>$liq > 0\<close>) ;;
+    if \<open>$zeroForOne\<close> \<medium_left_bracket>
+      getNextSqrtPriceFromAmount1_int ($sp, $liq, $amountOut, \<open>False\<close>)
+    \<medium_right_bracket>. \<medium_left_bracket>
+      getNextSqrtPriceFromAmount0_int ($sp, $liq, $amountOut, \<open>False\<close>)
     \<medium_right_bracket>. return (*it returns the result of the branch*)
   \<medium_right_bracket>. .
 
--- Uniswap_SwapMath.thy
+++ Uniswap_SwapMath.thy
@@ -1,7 +1,140 @@
 theory Uniswap_SwapMath
-  imports UniSWP_Common
+  imports Uniswap_SqrtPriceMath
 begin
 
 
+abbreviation Xor (infixr "XOR" 35) where \<open>A XOR B \<equiv> (A \<and> B \<or> \<not> A \<and> \<not> B)\<close>
+
+definition swap_step :: \<open>real \<Rightarrow> real \<Rightarrow> real \<Rightarrow> real \<Rightarrow> real \<Rightarrow> real \<times> real \<times> real \<times> real\<close>
+  \<comment> \<open>As described in the white paper.
+      Again we don't consider precision, so the specification can be simple and the difference
+        between exactIn and exactOut disappears.
+      The module of precision analysis will be designed after completing the \<phi>-system kernel
+        and Solidity semantics.\<close>
+  where \<open>swap_step price price_target L amount_remain \<gamma> = (
+    let zeroForOne = (price >= price_target) ; \<comment> \<open>whether using token0 to buy token1\<close>
+        exactIn = amount_remain \<ge> 0 ;
+        amount_remain' = if exactIn then amount_remain * (1 - \<gamma>) else amount_remain ;
+        max_amount = if zeroForOne XOR exactIn then L / price_target - L / price else L * (price_target - price) ;
+        \<Delta>amount = if exactIn then min amount_remain' max_amount else max amount_remain' max_amount ;
+        next_price = if zeroForOne XOR exactIn then (L / (L / price + \<Delta>amount)) else price + \<Delta>amount / L ;
+        amountIn = (if zeroForOne then L / next_price - L / price else L * (next_price - price)) ;
+          \<comment> \<open>L / price is the reserve amount of token0, and L * price is that of token 1\<close>
+        amountOut = (if zeroForOne then L * (price - next_price) else L / price - L / next_price) ;
+        fee = amountIn * \<gamma> / (1 - \<gamma>)
+     in (next_price, amountIn, amountOut, fee)
+)\<close>
+
+proc computeSwapStep:
+  input \<open>price \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> price_target \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> L \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> amount_remain \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> feePips \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  premises \<open>0 < price \<and> 0 < price_target \<and> 0 < L\<close>
+      and  \<open>0 < feePips \<and> feePips < 1\<close>
+  output \<open>let (next_price, amountIn, amountOut, fee) = swap_step price price_target L amount_remain feePips
+           in next_price \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> amountIn \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> amountOut \<Ztypecolon> \<v>\<a>\<l> \<real>\<heavy_comma> fee \<Ztypecolon> \<v>\<a>\<l> \<real>\<close>
+  is [routine]
+  \<medium_left_bracket>
+    var next_price, feeAmount ;;
+    \<open>0 \<Ztypecolon> \<real>\<close> \<open>0 \<Ztypecolon> \<real>\<close> \<rightarrow> var amountIn, amountOut (*TODO: support converge-of-branch between initialized and uninitialized variables*)
+    \<open>$price \<ge> $price_target\<close> \<rightarrow> val zeroForOne ;;
+    \<open>$amount_remain \<ge> 0\<close> \<rightarrow> val exactIn
+
+    (*the local definitions save us from long long long expressions.*)
+    define amount_remain' where \<open>amount_remain' = (if ?exactIn then amount_remain * (1 - feePips) else amount_remain)\<close>
+    define max_amount where \<open>max_amount =
+        (if ?zeroForOne XOR ?exactIn then L / price_target - L / price else L * (price_target - price))\<close>
+    define \<Delta>amount where \<open>\<Delta>amount = (if ?exactIn then min amount_remain' max_amount else max amount_remain' max_amount)\<close>
+    define next_price where \<open>next_price =
+        (if ?zeroForOne XOR ?exactIn then (L / (L / price + \<Delta>amount)) else price + \<Delta>amount / L)\<close> 
+    define amountIn where \<open>amountIn =
+        (if ?zeroForOne then L / next_price - L / price else L * (next_price - price))\<close>
+    define amountOut where \<open>amountOut =
+        (if ?zeroForOne then L * (price - next_price) else L / price - L / next_price)\<close> ;;
+
+    if \<open>$exactIn\<close> \<medium_left_bracket>
+      \<open>$amount_remain * (1 - $feePips)\<close> \<rightarrow> val amountRemainingLessFee ;;
+      if \<open>$zeroForOne\<close> \<medium_left_bracket> getAmount0Delta ($price_target, $price, $L) \<medium_right_bracket>.
+                       \<medium_left_bracket> getAmount1Delta ($price, $price_target, $L) \<medium_right_bracket>. \<rightarrow> amountIn ;;
+      if \<open>$amountRemainingLessFee \<ge> $amountIn\<close> \<medium_left_bracket>
+          $price_target \<rightarrow> next_price
+      \<medium_right_bracket>. \<medium_left_bracket>
+          getNextSqrtPriceFromInput ($price, $L, $amountRemainingLessFee, $zeroForOne) \<rightarrow> next_price
+      \<medium_right_bracket>.
+      is \<open>next_price\<close> 
+        affirm using \<phi> by (auto simp add: \<Delta>amount_def max_amount_def amount_remain'_def next_price_def)
+    \<medium_right_bracket>. \<medium_left_bracket>
+      if \<open>$zeroForOne\<close> \<medium_left_bracket> getAmount1Delta ($price_target, $price, $L) \<medium_right_bracket>.
+                       \<medium_left_bracket> getAmount0Delta ($price, $price_target, $L) \<medium_right_bracket>. \<rightarrow> amountOut ;;
+      if \<open>-$amount_remain \<ge> $amountOut\<close> \<medium_left_bracket>
+          $price_target \<rightarrow> next_price
+      \<medium_right_bracket>. \<medium_left_bracket>
+          getNextSqrtPriceFromOutput ($price, $L, neg ($amount_remain), $zeroForOne) ;;
+            affirm using \<phi> by (auto simp add: not_le)  (smt (verit) mult.commute pos_minus_divide_less_eq) ;; \<rightarrow> next_price
+      \<medium_right_bracket>. is \<open>next_price\<close>
+          affirm using \<phi> apply (auto simp add: not_le \<Delta>amount_def max_amount_def amount_remain'_def next_price_def)
+                          apply (smt (verit, best) mult_minus_right nonzero_mult_div_cancel_left)
+                          apply (smt (verit, best) mult_minus_right)
+                          apply (smt (verit, best) frac_le)
+                          by (metis max.absorb3 minus_diff_eq minus_less_iff mult_minus_right)
+    \<medium_right_bracket>. ;;
+
+    \<open>$price_target = $next_price\<close> \<rightarrow> val max ;;
+
+    have t1[simp]: \<open>?zeroForOne \<Longrightarrow> price_target \<le> next_price \<and> next_price \<le> price\<close>
+      unfolding \<Delta>amount_def max_amount_def amount_remain'_def next_price_def
+      using \<phi> apply (auto simp add: min_def max_def not_le)
+      apply (smt (verit) divide_pos_pos mult.commute mult_nonneg_nonneg pos_le_divide_eq)
+      apply (metis diff_ge_0_iff_ge diff_self divide_divide_eq_right linorder_not_less mult_pos_neg mult_zero_left mult_zero_right nat_arith.rule0 nonzero_mult_div_cancel_left order_antisym order_trans)
+      apply (smt (verit, del_insts) divide_pos_pos mult.commute pos_le_divide_eq split_mult_pos_le)
+      apply (smt (verit, ccfv_threshold) divide_pos_pos mult.commute mult_nonneg_nonneg pos_divide_le_eq)
+      apply (smt (verit, ccfv_SIG) mult.commute pos_le_divide_eq)
+      apply (simp add: divide_nonpos_pos)
+      apply (smt (verit, ccfv_SIG) divide_le_cancel nonzero_mult_div_cancel_left)
+      by (simp add: divide_nonpos_pos)
+
+    have t2[simp]: \<open>\<not> ?zeroForOne \<Longrightarrow> price \<le> next_price \<and> next_price \<le> price_target\<close>
+      unfolding \<Delta>amount_def max_amount_def amount_remain'_def next_price_def
+      using \<phi> apply (auto simp add: min_def max_def not_le)
+      using frac_less2 apply force
+      apply (smt (verit, ccfv_SIG) divide_pos_pos mult.commute pos_le_divide_eq)
+      apply (metis add.commute divide_nonneg_nonpos le_add_same_cancel2 less_diff_eq linorder_not_less mult.commute mult_imp_div_pos_le mult_imp_less_div_pos not_less_iff_gr_or_eq order_less_le_trans)
+      apply (smt (verit, best) mult.commute pos_divide_le_eq)
+      apply (smt (verit) mult_less_0_iff)
+      by (smt (verit, del_insts) zero_less_mult_iff) ;;
+
+    if \<open>$zeroForOne\<close> \<medium_left_bracket>
+      if \<open>$max \<and> $exactIn\<close> \<medium_left_bracket> $amountIn \<medium_right_bracket>. \<medium_left_bracket>
+        getAmount0Delta ($next_price, $price, $L) affirm using \<phi> t1 by (auto; linarith) ;; \<medium_right_bracket>.
+      \<rightarrow> amountIn is amountIn affirm unfolding amountIn_def using \<phi> by auto ;;
+      if \<open>$max \<and> \<not> $exactIn\<close> \<medium_left_bracket> $amountOut \<medium_right_bracket>. \<medium_left_bracket>
+        getAmount1Delta ($next_price, $price, $L) affirm using \<phi> t1 by (auto; linarith) ;; \<medium_right_bracket>.
+      \<rightarrow> amountOut is amountOut affirm unfolding amountOut_def using \<phi> by auto
+    \<medium_right_bracket>. \<medium_left_bracket>
+      if \<open>$max \<and> $exactIn\<close> \<medium_left_bracket> $amountIn \<medium_right_bracket>. \<medium_left_bracket>
+        getAmount1Delta ($price, $next_price, $L) \<medium_right_bracket>.
+      \<rightarrow> amountIn is amountIn affirm unfolding amountIn_def using \<phi> by auto ;;
+      if \<open>$max \<and> \<not> $exactIn\<close> \<medium_left_bracket> $amountOut \<medium_right_bracket>. \<medium_left_bracket>
+        getAmount0Delta ($price, $next_price, $L) \<medium_right_bracket>.
+      \<rightarrow> amountOut is amountOut affirm unfolding amountOut_def using \<phi> by auto
+    \<medium_right_bracket>. ;;
+  
+  have t3: \<open>\<not> ?exactIn \<Longrightarrow> amountOut \<le> - amount_remain\<close>
+    unfolding amountOut_def next_price_def \<Delta>amount_def amount_remain'_def max_amount_def
+    using \<phi> by auto
+  text \<open>Therefore, when there is no precision lost, the following branch will never be entered\<close> ;;
+
+  if \<open>\<not> $exactIn \<and> $amountOut > - $amount_remain\<close> \<medium_left_bracket>
+    \<open>- $amount_remain\<close> \<rightarrow> amountOut
+  \<medium_right_bracket>. \<medium_left_bracket> \<medium_right_bracket>. is amountOut affirm using t3 by auto ;;
+
+  if \<open>$exactIn \<and> $next_price \<noteq> $price_target\<close> \<medium_left_bracket>
+    \<open>$amount_remain - $amountIn\<close> \<rightarrow> feeAmount is \<open>amountIn * feePips / (1 - feePips)\<close>
+    affirm using \<phi> by (auto simp add: amountIn_def next_price_def \<Delta>amount_def amount_remain'_def max_amount_def min_def;
+                       simp add: right_diff_distrib' right_diff_distrib)
+  \<medium_right_bracket>. \<medium_left_bracket> \<open>$amountIn * $feePips / (1 - $feePips)\<close> \<rightarrow> feeAmount \<medium_right_bracket>. ;;
+
+  return ($next_price, $amountIn, $amountOut, $feeAmount)
+    affirm using \<phi> by (auto simp add: swap_step_def Let_def amountIn_def next_price_def \<Delta>amount_def
+        amount_remain'_def max_amount_def amountOut_def)
+  \<medium_right_bracket>. .
 
 end
